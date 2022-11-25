a741674a1629fbfa45a96e87120af0332670f819
iceqi <95857548@qq.com>
2022-11-25 13:48:34 +0800

    抖音交易系统api

--- src/Client.php
+++ src/Client.php
@@ -15,16 +15,7 @@ class Client
 
     public function __construct($options = [])
     {
-        $config = [
-            'appid' => 'tt08867e3b97b8ee8001', //AppID
-            'secret' => '482c1661421d091397de0ae9fbd7f60913a9ad07', //AppSecret
-            'salt' => '47jJYnYODbT782WPD0FlAHS5b4J8IeTYcsNGGtmX', //支付密钥值
-            'notify_url' => '', //支付回调地址
-            'thirdparty_id' => '', //第三方平台服务商 id，非服务商模式留空，暂时用不上
-            'merchant_id' => '70948675039435021170', //商户号
-            'commission' => '0.6', //抖音平台技术服务费，小程序平台在分账接点收取，已包含支付手续费
-        ];
-        $this->_config = array_merge($options, $config);
+        $this->_config = $options;
     }
 
     public function request(BaseApis $baseApis)
